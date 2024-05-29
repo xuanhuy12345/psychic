0803f11398d302a67d9165c19a880b192645664d
Lydia(Zi) Ye <143548312+Lydia-Ye@users.noreply.github.com>
2024-05-29 14:42:32 -0500

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -4,9 +4,20 @@ library(ggplot2)
 # Example data
 sampleData <- read_excel("MAP2024/PsychicApp Sample1 Data.xlsx")
 
-# Create histogram
-ggplot(sampleData, aes(x = Successes)) + 
-  geom_histogram(bins = 12, color = "white") +
-  geom_histogram(data = sampleData[sampleData$Successes >= 6,], bins = 12, color = "white", fill = "red") +
-  geom_vline(xintercept=6, color="red", linetype="dashed", size=1) +
-  annotate("text", x=6.3, y = 15, label="Num of Success ≥ 6", color="red", vjust=-1)
+psychic1 = sampleData[sampleData$NumCard == 2, ]
+psychic2 = sampleData[sampleData$NumCard == 5, ]
+
+# Create histogram for psychic1 using numbers
+ggplot(psychic1, aes(x = Successes)) + 
+  geom_histogram(bins = 8, color = "white") +
+  geom_histogram(data = psychic1[psychic1$Successes >= 4,], bins = 8, color = "white", fill = "light blue") +
+  geom_vline(xintercept=4, color="blue", linetype="dashed", size=1) +
+  annotate("text", x=4.1, y = 11, label="As extreme as (≥) 4", color="black", vjust=-1)
+
+
+# Create histogram for psychic2 using numbers
+ggplot(psychic2, aes(x = Successes)) + 
+  geom_histogram(bins = 8, color = "white") +
+  geom_histogram(data = psychic2[psychic2$Successes >= 4,], bins = 8, color = "white", fill = "light blue") +
+  geom_vline(xintercept=4, color="blue", linetype="dashed", size=1) +
+  annotate("text", x=4.1, y = 11, label="As extreme as (≥) 4", color="black", vjust=-1)
