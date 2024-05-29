518c9ee703e2bc96914efd271747376d36fdc4c0
Lydia(Zi) Ye <143548312+Lydia-Ye@users.noreply.github.com>
2024-05-29 14:11:46 -0500

    Update ggplot.R with a sample shaded histogram

--- ggplot.R
+++ ggplot.R
@@ -1,1 +1,12 @@
+# Load necessary library
+library(ggplot2)
 
+# Example data
+sampleData <- read_excel("MAP2024/PsychicApp Sample1 Data.xlsx")
+
+# Create histogram
+ggplot(sampleData, aes(x = Successes)) + 
+  geom_histogram(bins = 12, color = "white") +
+  geom_histogram(data = sampleData[sampleData$Successes >= 6,], bins = 12, color = "white", fill = "red") +
+  geom_vline(xintercept=6, color="red", linetype="dashed", size=1) +
+  annotate("text", x=6.3, y = 15, label="Num of Success ≥ 6", color="red", vjust=-1)
